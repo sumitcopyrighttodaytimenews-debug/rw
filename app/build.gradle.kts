8283194ce272acf5a83bc64554324a7fad42b84c
sumitcopyrighttodaytimenews-debug <sumitcopyrighttodaytimenews@gmail.com>
2026-06-20 00:08:38 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
+    
+      signingConfig = signingConfigs.getByName("debugConfig")
     }
   }
   compileOptions {
